9e223bc90591fec4713795747e79eae27965c4cd
Sivan <sun.sivan@gmail.com>
2018-10-19 17:44:58 +0800

    Aloha

--- es/index.js
+++
@@ -1,8 +0,0 @@
-import Index from './Index/index';
-import Cascad from './components/Cascad';
-import InfoBlock from './components/InfoBlock';
-import Logo from './components/Logo';
-import Login from './components/Login';
-
-export default Index;
-export { Cascad, InfoBlock, Logo, Login };
