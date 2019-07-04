c89f29025fa8247642d539725cf31f3216dc6a35
solarjoker <chenissolar@gmail.com>
2019-07-04 13:59:16 +0800

    解决侧边栏选中问题

--- src/theme/classic/index.js
+++ src/theme/classic/index.js
@@ -10,17 +10,8 @@ class Index extends Component {
   static type = 'classic'
 
   render () {
-    const {
-      collapse
-    } = this.state
-    let {
-      header,
-      routes,
-      logo,
-      footer,
-      config,
-      breadcrumb
-    } = this.props
+    const { collapse } = this.state
+    let { header, routes, logo, footer, config, breadcrumb } = this.props
     const layoutClasses = classNames(
       'layout',
       'layout--classic',
@@ -34,7 +25,7 @@ class Index extends Component {
       breadcrumb,
       footer
     }
-
+    console.log('>>>>>>',layoutClasses)
     return (
       <div className={layoutClasses}>
         <Header header={header} logo={logo} />
