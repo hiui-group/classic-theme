56e299cdebd59870ff769bcd48bd63b402147902
chenxian <chenxian@xiaomi.com>
2019-08-02 17:55:27 +0800

    更新示例代码

--- examples/theme/routes-config.js
+++ examples/theme/routes-config.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Mi = () => <div>小米手机</div>
 const RedMi = () => <div>红米手机</div>
 const BlackShark = () => <div>黑鲨手机</div>
 const TV = () => <div>小米电视</div>
-const SoundBox = () => <div>小米音响</div>
+const SoundBox = () => (
+  <div>
+    小米音响<Link to='/robot-detail'>去详情页</Link>
+  </div>
+)
 const Robot = () => <div>米家扫地机器人</div>
+const RobotDetail = () => <div>米家扫地机器人详情页</div>
 
 const config = [
   { name: '电视', path: '/tv', component: TV },
@@ -21,7 +27,8 @@ const config = [
     name: '智能硬件',
     children: [
       { name: '音响', path: '/audio', component: SoundBox },
-      { name: '扫地机器人', path: '/robot', component: Robot }
+      { name: '扫地机器人', path: '/robot', component: Robot },
+      { path: '/robot-detail/:id', component: RobotDetail }
     ]
   }
 ]
