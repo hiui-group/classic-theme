6978fcf719a571d49df2bbe2a5a4434403661914
solarjoker <chenissolar@gmail.com>
2020-06-29 18:53:42 +0800

    feat: add switch transition

--- src/components/Sider/NormalMenu.jsx
+++ src/components/Sider/NormalMenu.jsx
@@ -4,7 +4,15 @@ import './style/index.scss'
 import { Icon } from '@hi-ui/hiui'
 import Expander from './Expander'
 
-const NormalMenu = ({expandedId, menu, setExpandedId, selectedMenus, level, renderChildren, onSelectMenu}) => {
+const NormalMenu = ({
+  expandedId,
+  menu,
+  setExpandedId,
+  selectedMenus,
+  level,
+  renderChildren,
+  onSelectMenu
+}) => {
   const [expanded, setExpanded] = useState(false)
 
   useEffect(() => {
@@ -15,44 +23,57 @@ const NormalMenu = ({expandedId, menu, setExpandedId, selectedMenus, level, rend
     }
   }, [expandedId])
 
-  return <div className={'sider__menu-item'} key={menu.id}>
-    <div
-      onClick={() => {
-        let _expandedId = [...expandedId]
-        if (_expandedId.includes(menu.id)) {
-          _expandedId = _expandedId.filter((id) => id !== menu.id)
-        } else {
-          _expandedId.push(menu.id)
-        }
-        setExpandedId(_expandedId)
-        setExpanded(!expanded)
-        if (menu.path) {
-          onSelectMenu(menu)
-        }
-      }}
-      style={{ paddingLeft: level * 16 }}
-      className={classNames('menu__title', {
-        'menu__title--active':
-        selectedMenus &&
-        selectedMenus.map((sm) => sm.id).includes(menu.id) &&
-        menu.id !==
-          (selectedMenus && selectedMenus.map((sm) => sm.id)[selectedMenus.length - 1]),
-        'menu__leaf-title--active':
-        menu.id ===
-        (selectedMenus && selectedMenus.map((sm) => sm.id)[selectedMenus.length - 1])
-      })}
-    >
-      <span>
-        <Icon name={menu.icon || 'user'} />
-        { menu.name}
-      </span>
+  return (
+    <div className={'sider__menu-item'} key={menu.id}>
+      <div
+        onClick={() => {
+          let _expandedId = [...expandedId]
+          if (_expandedId.includes(menu.id)) {
+            _expandedId = _expandedId.filter((id) => id !== menu.id)
+          } else {
+            _expandedId.push(menu.id)
+          }
+          setExpandedId(_expandedId)
+          setExpanded(!expanded)
+          if (menu.path) {
+            onSelectMenu(menu)
+          }
+        }}
+        style={{ paddingLeft: level * 16 }}
+        className={classNames('menu__title', {
+          'menu__title--active':
+            selectedMenus &&
+            selectedMenus.map((sm) => sm.id).includes(menu.id) &&
+            menu.id !==
+              (selectedMenus && selectedMenus.map((sm) => sm.id)[selectedMenus.length - 1]),
+          'menu__leaf-title--active':
+            menu.id ===
+            (selectedMenus && selectedMenus.map((sm) => sm.id)[selectedMenus.length - 1])
+        })}
+      >
+        <span>
+          <Icon name={menu.icon || 'user'} />
+          {menu.name}
+        </span>
 
+        {menu.children && menu.children.length > 0 && (
+          <Icon
+            style={{
+              display: 'inline-block',
+              transition: 'transform 0.3s',
+              transform: expandedId.includes(menu.id) ? 'rotate(180deg)' : 'rotate(0)'
+            }}
+            name={'down'}
+          />
+        )}
+      </div>
       {menu.children && menu.children.length > 0 && (
-        <Icon name={expandedId.includes(menu.id) ? 'up' : 'down'} />
+        <Expander expanded={expanded}>
+          {renderChildren(menu.children, selectedMenus, level + 1, expandedId)}
+        </Expander>
       )}
     </div>
-    {menu.children && menu.children.length > 0 && <Expander expanded={expanded} >{renderChildren(menu.children, selectedMenus, level + 1, expandedId)}</Expander>}
-  </div>
+  )
 }
 
 export default NormalMenu
