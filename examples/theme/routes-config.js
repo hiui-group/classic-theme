15a7ab65614e85e61d7bdf547034f0bbae2fb3c0
pengrui3 <pengrui3@xiaomi.com>
2021-10-12 11:45:47 +0800

    fix: 修复路由删除

--- examples/theme/routes-config.js
+++ examples/theme/routes-config.js
@@ -16,7 +16,9 @@ const BlackShark = (props) => {
 // const TV = () => <div>小米电视</div>
 const SoundBox = () => (
   <div>
-    小米音响<Link to="/robot-detail/1">去详情页</Link>
+    小米音响<Link to="/robot-detail/1">去详情页1</Link>
+    小米音响<Link to="/robot-detail/2">去详情页2</Link>
+    小米音响<Link to="/robot-detail/3">去详情页3</Link>
   </div>
 )
 const Robot = () => <div>米家扫地机器人</div>
--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -7,7 +7,7 @@ import './style/index.js'
 const prefix = 'theme-nav-bar'
 const Tag = ({ menu, history, onMenuClick }) => {
   const [historyPaths, setHistoryPaths] = useState({})
-  const [activePath, setActivePath] = useState({})
+  const [activePath, setActivePath] = useState('')
   const mergeRouter = useRef()
   useEffect(() => {
     // 默认进入执行一次
@@ -27,6 +27,7 @@ const Tag = ({ menu, history, onMenuClick }) => {
       }
       setActivePath(pathname)
     })
+
     return () => {
       unlistten()
     }
@@ -34,18 +35,32 @@ const Tag = ({ menu, history, onMenuClick }) => {
   useEffect(() => {
     mergeRouter.current = _.cloneDeep(historyPaths)
   }, [historyPaths])
+
+  const [nextPath, setNextPath] = useState(activePath)
+
   const deleteItem = useCallback(
     (path, prePath, nextPath, isActive) => {
       const _historyPaths = _.cloneDeep(historyPaths)
       delete _historyPaths[path]
       setHistoryPaths(_historyPaths)
+
       if (isActive) {
-        prePath ? setActivePath(prePath) : setActivePath(nextPath)
+        const _nextPath = prePath || nextPath || ''
+        // 在下一render阶段设置pathname
+        setNextPath(_nextPath)
       }
     },
     [historyPaths, activePath]
   )
+
+  useEffect(() => {
+    if (activePath !== nextPath) {
+      history.replace(nextPath)
+    }
+  }, [nextPath])
+
   const historyPathsKeys = Object.keys(historyPaths)
+  const isTabAlone = historyPathsKeys.length === 1
 
   return (
     <div className={`${prefix}`}>
@@ -64,30 +79,32 @@ const Tag = ({ menu, history, onMenuClick }) => {
             }}
           >
             <span>{name}</span>
-            <span
-              className={`${prefix}_icon`}
-              onClick={(e) => {
-                e.stopPropagation()
-                deleteItem(path, prePath, nextPath, isActive)
-              }}
-            >
-              <svg
-                t="1624244997818"
-                className="icon"
-                viewBox="0 0 1024 1024"
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-                p-id="8674"
-                width="14"
-                height="14"
+            {isTabAlone ? null : (
+              <span
+                className={`${prefix}_icon`}
+                onClick={(e) => {
+                  e.stopPropagation()
+                  deleteItem(key, prePath, nextPath, isActive)
+                }}
               >
-                <path
-                  d="M834.44 234.814a8 8 0 0 1 0 11.314L568.568 512 834.44 777.872a8 8 0 0 1 0 11.314l-45.254 45.254a8 8 0 0 1-11.314 0L512 568.568 246.128 834.44a8 8 0 0 1-11.314 0L189.56 789.186a8 8 0 0 1 0-11.314L455.432 512 189.56 246.128a8 8 0 0 1 0-11.314l45.254-45.254a8 8 0 0 1 11.314 0L512 455.43 777.872 189.56a8 8 0 0 1 11.314 0l45.254 45.254z"
-                  p-id="8675"
-                  fill="#333"
-                ></path>
-              </svg>
-            </span>
+                <svg
+                  t="1624244997818"
+                  className="icon"
+                  viewBox="0 0 1024 1024"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  p-id="8674"
+                  width="14"
+                  height="14"
+                >
+                  <path
+                    d="M834.44 234.814a8 8 0 0 1 0 11.314L568.568 512 834.44 777.872a8 8 0 0 1 0 11.314l-45.254 45.254a8 8 0 0 1-11.314 0L512 568.568 246.128 834.44a8 8 0 0 1-11.314 0L189.56 789.186a8 8 0 0 1 0-11.314L455.432 512 189.56 246.128a8 8 0 0 1 0-11.314l45.254-45.254a8 8 0 0 1 11.314 0L512 455.43 777.872 189.56a8 8 0 0 1 11.314 0l45.254 45.254z"
+                    p-id="8675"
+                    fill="#333"
+                  ></path>
+                </svg>
+              </span>
+            )}
           </span>
         )
       })}
